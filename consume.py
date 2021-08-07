803de096895d04f8db70c1ff850d751f179d0204
restureese <restureese@gmail.com>
2021-08-07 23:45:55 +0700

    delay messaging

---
+++ consume.py
@@ -0,0 +1,29 @@
+"""
+    @Author varx
+    @Date 07/08/21 21.42
+    @Describe 
+"""
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='payment',
+                         exchange_type='x-delayed-message',
+                         arguments={"x-delayed-type":"direct"})
+
+result = channel.queue_declare(queue='payment',exclusive=True)
+queue_name = result.method.queue
+
+
+channel.queue_bind(exchange='payment',
+                   queue=queue_name,
+                   routing_key='scheduler')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(queue_name, callback)
+channel.start_consuming()
---
+++ publish.py
@@ -0,0 +1,21 @@
+"""
+    @Author varx
+    @Date 07/08/21 21.42
+    @Describe 
+"""
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='payment',
+                         exchange_type='x-delayed-message',
+                         arguments={"x-delayed-type":"direct"})
+
+message = 'Hello World! 10 detik'
+channel.basic_publish(exchange='payment',
+                      routing_key='scheduler',
+                      properties=pika.BasicProperties(headers={'x-delay': 10000}),
+                      body=message)
+connection.close()
